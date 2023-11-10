66f453ad037e472874c9416bc4d940d1797a421c
Sam Chan <sam.chan@oocl.com>
2023-11-11 05:58:45 +0800

    (feat): implement add function to parse numbers from strings

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -2,6 +2,18 @@ package com.tw;
 
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        if (string.isEmpty()) {
+            return 0;
+        }
+        String[] numbers = string.split("[,\n/;]");
+
+        int sum = 0;
+        for (String num : numbers) {
+            if (!num.isEmpty()) {
+                sum += Integer.parseInt(num);
+            }
+        }
+
+        return sum;
     }
 }
